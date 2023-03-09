8ebc498ccecee89a67c95b82e75449d80d157bca
Clem O Regan <clem@example.com>
2023-03-09 11:16:12 +0100

    Added TLS namespace for apps

--- terraform/cluster_tls_local.tf
+++ terraform/cluster_tls_local.tf
@@ -1,4 +1,5 @@
 # Local deploy of Lets encrypt CA generated traefik certificates - post cluster create
+# Creating namespace and secret for tools: traefik, harbor, rancher etc & apps: for custom or services apps
 resource "null_resource" "cluster_tls" {
 
   depends_on = [kind_cluster.default]
@@ -9,9 +10,11 @@ resource "null_resource" "cluster_tls" {
       set -e
       echo 'Applying TLS Config with kubectl...'
       kubectl create namespace tools
+      kubectl create namespace apps
       kubectl -n tools create configmap traefik-cert --from-file=../certs/fullchain.pem
       kubectl -n tools create configmap traefik-key --from-file=../certs/privkey.pem
       kubectl -n tools create secret tls traefik-tls-cert --key=../certs/privkey.pem --cert=../certs/fullchain.pem
+      kubectl -n apps create secret tls traefik-tls-cert --key=../certs/privkey.pem --cert=../certs/fullchain.pem
     EOT
   }
 }
